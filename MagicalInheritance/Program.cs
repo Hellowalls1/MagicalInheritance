143ed72458b010a1711cc39c5aad85282f630638
Hellowalls1 <thesingingbutcher@gmail.com>
2020-09-20 20:05:39 -0500

    edit

--- MagicalInheritance/Program.cs
+++ MagicalInheritance/Program.cs
@@ -6,29 +6,41 @@ namespace MagicalInheritance
     {
         static void Main(string[] args)
         {
-            //creating a new Pupil class which sets the title
-            Pupil p = new Pupil("Mezil-kree");
 
-            //creating a new Storm class and giving it the value of a pupil (p) with the parameters that CastWindStorm holds in the Pupil class
+            Console.WriteLine("Welcome to Magical Inheritance. A place where magical beings do some crazy shit.");
+            Console.WriteLine("Are you ready to rumble? Enter yes or no.");
+            string userAnswer = Console.ReadLine();
 
-            Storm windStorm = p.CastWindStorm();
+            if (userAnswer == "no")
+            {
+                Console.WriteLine("Your presense is not needed plebe! Go back to the village from whenst you came!");
+            }
+            else
+            {
+                //creating a new Pupil class which sets the title
+                Pupil p = new Pupil("Mezil-kree");
 
-            //announcing the windstorm utilizing the parameter values from CastWindStorm
-            Console.WriteLine(windStorm.Announce());
+                //creating a new Storm class and giving it the value of a pupil (p) with the parameters that CastWindStorm holds in the Pupil class
 
-            Mage m = new Mage("Gul'dan");
-            Storm mageWindStorm = m.CastWindStorm();
-            Storm rainStorm = m.CastRainStorm();
-            Console.WriteLine(mageWindStorm.Announce());
-            Console.WriteLine(rainStorm.Announce());
+                Storm windStorm = p.CastWindStorm();
 
-            Archmage am = new Archmage("Nielas Aran");
-            Storm amRainStorm = am.CastRainStorm();
-            Storm lightningStorm = am.CastLightningStorm();
-            Storm amWindStorm = am.CastWindStorm();
-            Console.WriteLine(amWindStorm.Announce());
-            Console.WriteLine(amRainStorm.Announce());
-            Console.WriteLine(lightningStorm.Announce());
+                //announcing the windstorm utilizing the parameter values from CastWindStorm
+                Console.WriteLine(windStorm.Announce());
+
+                Mage m = new Mage("Gul'dan");
+                Storm mageWindStorm = m.CastWindStorm();
+                Storm rainStorm = m.CastRainStorm();
+                Console.WriteLine(mageWindStorm.Announce());
+                Console.WriteLine(rainStorm.Announce());
+
+                Archmage am = new Archmage("Nielas Aran");
+                Storm amRainStorm = am.CastRainStorm();
+                Storm lightningStorm = am.CastLightningStorm();
+                Storm amWindStorm = am.CastWindStorm();
+                Console.WriteLine(amWindStorm.Announce());
+                Console.WriteLine(amRainStorm.Announce());
+                Console.WriteLine(lightningStorm.Announce());
+            }
         }
     }
 }
